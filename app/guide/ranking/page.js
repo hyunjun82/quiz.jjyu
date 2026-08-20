7311834d28e930dcefcaca3d44532395683d7a8b
hyunjun82 <33han58@gmail.com>
2026-08-20 09:19:19 +0900

    Remove AdUnit component from ranking page

--- app/guide/ranking/page.js
+++ app/guide/ranking/page.js
@@ -74,7 +74,6 @@ export default function RankingGuide() {
         <b>{Math.round(sumQuestions)}개</b>입니다.
       </p>
 
-      <AdUnit slot="3353452458" />
       <h2>몇 개까지 하면 얼마가 되나</h2>
       <p>
         앱을 전부 깔 필요는 없습니다. 위에서부터 몇 개까지 했을 때 얼마가 되는지 계산했습니다.
